1e842c7c8929dc16f4c0f628158dd08c9add8830
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-07-28 21:45:37 -0400

    feat: subscribe mail endpoint

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,3 +1,4 @@
+use models::{Mail, MailRequest};
 use worker::*;
 
 // This event is called on start worker
@@ -17,5 +18,33 @@ fn start() {
 //
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-    Router::new().run(req, env).await
+    Router::new()
+        .post_async("/subscribe", subscribe)
+        .run(req, env)
+        .await
+}
+
+async fn subscribe(mut req: Request, ctx: RouteContext<()>) -> worker::Result<Response> {
+    let db = ctx.d1("mail-subscription")?;
+    let agent = req
+        .headers()
+        .get("Agent")?
+        .expect("Se necesita un agent para la solicitud");
+    let country = req
+        .cf()
+        .expect("No se pudieron obtener los valores de cloudflare")
+        .country()
+        .unwrap_or("Unknown".to_string());
+
+    let req = req.json::<MailRequest>().await?;
+
+    let mail = Mail::new(req, &country, &agent);
+    let res = mail.insert(&db).await?;
+    
+    if res {
+        return Response::ok("");
+    }
+
+    Response::error("Ha ocurrido un error", 500)
+
 }
